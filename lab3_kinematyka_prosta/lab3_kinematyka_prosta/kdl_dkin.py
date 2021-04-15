a04da83b14a4e4fc1af4284fbfaf359d7d378500
janek <kan.jan@wp.pl>
2021-04-15 21:33:26 +0200

    Publisher + 2x subscriber

--- lab3_kinematyka_prosta/lab3_kinematyka_prosta/kdl_dkin.py
+++ lab3_kinematyka_prosta/lab3_kinematyka_prosta/kdl_dkin.py
@@ -3,6 +3,8 @@ from PyKDL import *
 # Zarys/baza to pliku kdl_dkin.py
 # Struktura drzewa podobna do urdf
 
+# Dojdzie jeszcze czytanie z YAML
+
 print("Creating Robotic Chain")
 
 chain = Chain()
@@ -25,22 +27,26 @@ chain.addSegment(segment3)
 
 # Tu jest bug, że nie akceptuje Joint.None
 # https://github.com/ros/kdl_parser/issues/44
-link_3__tool = Joint(Joint.None) 
-frame4 = Frame(Rotation.RPY(0,1,0),Vector(0.2,0.3,0))
-segment4 = Segment(link_3__tool,frame4)
-chain.addSegment(segment4)
+# link_3__tool = Joint(Joint.None) 
+# frame4 = Frame(Rotation.RPY(0,1,0),Vector(0.2,0.3,0))
+# segment4 = Segment(link_3__tool,frame4)
+# chain.addSegment(segment4)
+
+# Możemy to ominąć i dodać offset w postaci końcówki w osi z 
+# do końcowego wyniku ( bo orientacja końcówki jest zgodna z orientacją
+# ostatniego członu)
 
 
 
 print("Forward kinematics")
 
-jointAngles=JntArray(3)
-jointAngles[0]=0.5236
-jointAngles[1]=0.5236
-jointAngles[2]=-1.5708
+joint_positions=JntArray(3)
+joint_positions[0]=0.5236
+joint_positions[1]=0.5236
+joint_positions[2]=-1.5708
 fk=ChainFkSolverPos_recursive(chain)
 finalFrame=Frame()
-fk.JntToCart(jointAngles,finalFrame)
+fk.JntToCart(joint_positions,finalFrame)
 print("Rotational Matrix of the final Frame: ")
 print(finalFrame.M)
 print("End-effector position: ",finalFrame.p)
