e28689091b115c9b1b8f9690b8f29cfe34e95109
janek <kan.jan@wp.pl>
2021-05-17 23:25:17 +0200

    Dodanie węzła IKIN

--- lab5_kinematyka_odwrotna/lab5_kinematyka_odwrotna/ikin.py
+++ lab5_kinematyka_odwrotna/lab5_kinematyka_odwrotna/ikin.py
@@ -10,16 +10,24 @@ from geometry_msgs.msg import PoseStamped
 from tf2_ros import TransformBroadcaster, TransformStamped
 import json
 from rclpy.clock import ROSClock
-from PyKDL import *
-import yaml
 
 
+# Obsługa kinematyki odwrotnej
 
 class ikin(Node):
 
-	def __init__(self):
+	def __init__(self, DHdata):
+		# DHdata to wartości d z tabeli DH
+		
 		super().__init__('ikin')
 
+		# Parametry D z tabeli
+		# Jest to tutaj, aby nie było parsowania z .json za każdym razem wywołania callback'u
+		self.dh0 = DHdata[0];
+		self.dh1 = DHdata[1];
+		self.dh2 = DHdata[2];
+
+		# Rozkazy dla węzła IKIN pochodzą z węzła interpolacji trajektorii w przestrzeni operacyjnej
 		self.subscription = self.create_subscription(
 			PoseStamped(),
 			'/pose_op_interpolation',
@@ -30,11 +38,51 @@ class ikin(Node):
 
 	def listener_callback(self, msg):
 
-		joint1 = msg.position[1]
-		joint2 = msg.position[2]
-		joint3 = msg.position[3]
+		# Pobieramy wartości przesunięć w złączach
+		joint1 = msg.position[0]
+		joint2 = msg.position[1]
+		joint3 = msg.position[2]
 
-		d_values_from_DH = []
+
+
+		# obsługa kinematyki odwrotnej
+		# (x, y, z) -> położenie w przestrzeni kartezjańskiej
+		x = self.dh2 - joint3
+		y = -self.dh1 + joint2
+		z = self.dh0 - joint1
+
+
+		# Warningi
+		if( not (x < 0 & x > -self.dh2)):
+			self.get_logger().warn('Position out of limit')
+		else if(not (y < 0 & y > -self.dh1)):
+			self.get_logger().warn('Position out of limit')
+		else if(not (z < 0 & z > -self.dh0)):
+			self.get_logger().warn('Position out of limit')
+		else:
+			# Publikujemy na /joint_states TYLKO jeśli nie ma warningów
+			qos_profile = QoSProfile(depth=10)
+			self.joint_pub = self.create_publisher(JointState, '/joint_states', qos_profile)
+			joint_state = JointState()
+			now = self.get_clock().now()
+			joint_state.header.stamp = now.to_msg()
+			joint_state.name = ['base_link__link1', 'link1__link2', 'link2__link3']
+			joint_state.position = [x, y, z]
+			
+			self.joint_pub.publish(joint_state)
+
+
+
+# czytanie parametrów tablicy DH z pliku
+def readDHfile():
+
+    with open(os.path.join(
+        get_package_share_directory('lab5_kinematyka_odwrotna'),'Tablica_MD-H.json'), 'r') as file:
+
+        values = json.loads(file.read())
+
+        # Wektorr kolejnych wartości d z tabeli DH
+       	d_values_from_DH = []
 
 
         for i, mark in enumerate(values.keys()):
@@ -45,47 +93,7 @@ class ikin(Node):
             d_values_from_DH.append(d)
 
 
-
-
-		##obsługa kinematyki odwrotnej
-		x = d_values_from_DH[2] - joint3
-		y = -d_values_from_DH[1] + joint2
-		z = d_values_from_DH[0] - joint1
-
-
-		if( not (x < 0 & x > -d_values_from_DH[2])):
-			send_warning()
-		else if(not (y < 0 & y > -d_values_from_DH[1])):
-			send_warning()
-		else if(not (z < 0 & z > -d_values_from_DH[0])):
-			send_warning()
-
-
-
-
-		qos_profile = QoSProfile(depth=10)
-		self.joint_pub = self.create_publisher(JointState, '/joint_states', qos_profile)
-		joint_state = JointState()
-		now = self.get_clock().now()
-		joint_state.header.stamp = now.to_msg()
-		joint_state.name = ['base_link__link1', 'link1__link2', 'link2__link3']
-		joint_state.position = [x, y, z]
-		
-		self.joint_pub.publish(joint_state)
-
-	def send_warning(self):
-		self.ikin.get_logger().warn('Nie można przejść do żądanego położenia z podwodu mechanicznych ograniczeń manipulatora')
-
-
-# czytanie parametrów tablicy DH z pliku
-def readDHfile():
-
-    with open(os.path.join(
-        get_package_share_directory('lab4_kinematyka_odwrotna'),'Tablica_MD-H.json'), 'r') as file:
-
-        values = json.loads(file.read())
-
-    return values
+    return d_values_from_DH
 
 
 
@@ -95,7 +103,7 @@ def main(args=None):
 
 	rclpy.init(args=args)
 
-	ikin = ikin()
+	ikin = ikin(values)
 	rclpy.spin(ikin)
 
 	ikin.destroy_node()
