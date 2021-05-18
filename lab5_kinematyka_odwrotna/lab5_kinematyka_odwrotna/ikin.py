986dce0aa746e4eef3b759cc366ac3b69c27a2e2
Przemek <przemekdaniel@interia.eu>
2021-05-18 21:01:05 +0200

    Wersja chyba stabilna

--- lab5_kinematyka_odwrotna/lab5_kinematyka_odwrotna/ikin.py
+++ lab5_kinematyka_odwrotna/lab5_kinematyka_odwrotna/ikin.py
@@ -10,6 +10,8 @@ from geometry_msgs.msg import PoseStamped
 from tf2_ros import TransformBroadcaster, TransformStamped
 import json
 from rclpy.clock import ROSClock
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
 from PyKDL import *
 import yaml
 
@@ -20,6 +22,7 @@ class Ikin(Node):
 	def __init__(self):
 		super().__init__('ikin')
 		self.values = readDHfile()
+		self.markerArray = MarkerArray()
 
 		self.subscription = self.create_subscription(
 			PoseStamped(),
@@ -28,6 +31,9 @@ class Ikin(Node):
 			10)
 		self.subscription  
 
+
+
+
 	def send_warning(self):
 		self.ikin.get_logger().warn('Nie można przejść do żądanego położenia z podwodu mechanicznych ograniczeń manipulatora')
 
@@ -35,14 +41,37 @@ class Ikin(Node):
 
 	def listener_callback(self, msg):
 
-		joint1 = msg.pose.position.x
+		joint1 = msg.pose.position.z
 		joint2 = msg.pose.position.y
-		joint3 = msg.pose.position.z
+		joint3 = msg.pose.position.x
 		print("joint1: ", joint1)
 		print("joint2: ", joint2)
 		print("joint3: ", joint3)
 
 		d_values_from_DH = []
+		# Obsługa markerów
+		
+		qos_profile1 = QoSProfile(depth=10)
+		self.marker_pub = self.create_publisher(MarkerArray, '/marker', qos_profile1)
+		marker = Marker()
+		marker.header.frame_id = "base_link"
+
+		marker.id = 0
+		marker.action = Marker.DELETEALL
+		self.markerArray.markers.append(marker)
+
+		self.marker_pub.publish(self.markerArray)
+
+		marker.type = marker.SPHERE
+		marker.action = marker.ADD
+		marker.scale.x = 0.05
+		marker.scale.y = 0.05
+		marker.scale.z = 0.05
+		marker.color.a = 1.0
+		marker.color.r = 0.5
+		marker.color.g = 0.5
+		marker.color.b = 0.5
+
 
 
 		for i, mark in enumerate(self.values.keys()):
@@ -84,9 +113,26 @@ class Ikin(Node):
 		now = self.get_clock().now()
 		joint_state.header.stamp = now.to_msg()
 		joint_state.name = ['base_link__link1', 'link1__link2', 'link2__link3']
-		joint_state.position = [x, y, z]
+		joint_state.position = [z, y, x]
 		
 		self.joint_pub.publish(joint_state)
+
+
+		# Przypisanie wartości dla markerów
+		marker.pose.position.x = 2 + float(x)
+		marker.pose.position.y = -3 - float(y)
+		marker.pose.position.z = 2 + float(z)
+
+		# Obsługa tablicy markerów
+		self.markerArray.markers.append(marker)
+
+		id = 0
+		for m in self.markerArray.markers:
+			m.id = id
+			id += 1
+
+		#Publikowanie tablicy markerów
+		self.marker_pub.publish(self.markerArray)
 		print("publik")
 
 
